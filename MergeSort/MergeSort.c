536ffeb41903d642b7a1b9f17a523b443b3c7f4b
md_rajib_rari <rajibhossen688@gmail.com>
2022-11-29 12:56:00 +0600

    Merge Sort modify

--- MergeSort/MergeSort.c
+++ MergeSort/MergeSort.c
@@ -4,10 +4,12 @@
 int a[11] = { 10, 14, 19, 26, 27, 31, 33, 35, 42, 44, 0 };
 int b[10];
 
-void merging(int low, int mid, int high) {
+void merging(int low, int mid, int high)
+{
    int l1, l2, i;
 
-   for(l1 = low, l2 = mid + 1, i = low; l1 <= mid && l2 <= high; i++) {
+   for(l1 = low, l2 = mid + 1, i = low; l1 <= mid && l2 <= high; i++)
+   {
       if(a[l1] <= a[l2])
          b[i] = a[l1++];
       else
@@ -24,30 +26,36 @@ void merging(int low, int mid, int high) {
       a[i] = b[i];
 }
 
-void sort(int low, int high) {
+void sort(int low, int high)
+{
    int mid;
 
-   if(low < high) {
+   if(low < high)
+   {
       mid = (low + high) / 2;
       sort(low, mid);
       sort(mid+1, high);
       merging(low, mid, high);
-   } else {
+   }
+   else
+   {
       return;
    }
+
 }
 
-int main() {
+int main()
+{
    int i;
 
-   printf("List Before sorting\n");
+   printf("List Before sorting : ");
 
    for(i = 0; i <= max; i++)
       printf("%d ", a[i]);
 
    sort(0, max);
 
-   printf("\nList After sorting\n");
+   printf("\nList After sorting : ");
 
    for(i = 0; i <= max; i++)
       printf("%d ", a[i]);
